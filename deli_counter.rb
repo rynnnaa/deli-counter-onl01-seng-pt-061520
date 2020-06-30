ac6fe161af3fd8d204ce6408773a193a2f463760
Ryna Truong <rynatruong@outlook.com>
2020-06-30 22:56:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else 
+    phrase = katz_deli.each_with_index.map do |person, idx|
+    "#{idx + 1}. #{person}"
+    end
+    puts "The line is currently: #{phrase.join(' ')}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  
+end
+  
